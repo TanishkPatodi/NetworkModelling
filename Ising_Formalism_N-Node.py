0ffdb3a6493fac7411d69400e5ce511921a75385
TanishkPatodi <135834227+TanishkPatodi@users.noreply.github.com>
2023-09-24 00:30:23 +0530

    Edited to stop at steady state Edited to stop at steady state

--- Ising_Formalism_N-Node.py
+++ Ising_Formalism_N-Node.py
@@ -1,9 +1,12 @@
 #%%
 import numpy as np
-from itertools import product
+from itertools import product, chain
 import pandas as pd
+import random
 # %%
+# Defining functions
 def next_state_function(current_state_matrix, edge_matrix):
+       global current_state
        current_state_matrix_new = np.dot(edge_matrix, current_state_matrix)
        current_state_matrix_new_2 = [
               [-1 if element[0] < -1 else 
@@ -12,8 +15,19 @@ def next_state_function(current_state_matrix, edge_matrix):
               else element[0]] 
               for i, element in enumerate(current_state_matrix_new)
               ]
-       return current_state_matrix_new_2
+       node = random.randint(0, len(current_state)-1) #for async update
+       current_state_matrix[node] = current_state_matrix_new_2[node] # for async 
+       current_state = list(chain.from_iterable(current_state_matrix))
+       return current_state_matrix #For async update
+      #  return current_state_matrix_new_2 #For sync update
+def are_last_10_elements_same(lst):
+    if len(lst) < 10:
+        return False
+    
+    last_10_elements = lst[-10:]
+    return all(element == last_10_elements[0] for element in last_10_elements)
 
+#%%
 #Running simulations
 #taking the input for number of nodes in the system
 n = int(input('Enter the number of nodes in a system'))
@@ -39,17 +53,25 @@ for k in range(len(int_combinations)):
     states[str_combinations[k]] = 0 
     current_state_copy = current_state
     for i in range(100):
-       #  state_path.append(list(current_state))
+        state_path = []
         result = ','.join(map(str, current_state))
-        states.loc[result, str_combinations[k]] += 1
+      #   states.loc[result, str_combinations[k]] += 1
         current_state = current_state_copy
         for j in range(99): 
             next_state = next_state_function(current_state_matrix, edge_matrix) 
             result = ','.join(str(element[0]) for element in next_state)
-
-            states.loc[result, str_combinations[k]] += 1
+            # states.loc[result, str_combinations[k]] += 1
             current_state = [element[0] for element in next_state]
+            state_path.append(list(current_state))
             current_state_matrix = next_state
+            if are_last_10_elements_same(state_path):
+                states.loc[result, str_combinations[k]] += 1
+                break
+            else:
+                continue
+            
+            
+
        #      state_path.append(list(current_state))
     # print(current_state_copy)
 print(states)
--- N-node-toggle-SA_1D.py
+++ N-node-toggle-SA_1D.py
@@ -5,6 +5,12 @@ import random
 import pandas as pd
 from itertools import product
 #%%
+def are_last_10_elements_same(lst):
+    if len(lst) < 10:
+        return False
+    
+    last_10_elements = lst[-10:]
+    return all(element == last_10_elements[0] for element in last_10_elements)
 #Defining function to obtain a new state
 def run(x):
     x_all = []
@@ -22,7 +28,7 @@ def n_node(list_of_nodes, length):
                         for i, element in enumerate(list_of_nodes[0])]
     list_of_nodes[1] = list_of_nodes[1] # For removing self activation
     # Apply AND operation between each element and the rest of the elements
-    list_of_nodes[2][node] = any(list_of_nodes[1]) #Change any to all()/any() for async AND/OR
+    list_of_nodes[2][node] = all(list_of_nodes[1]) #Change any to all()/any() for async AND/OR
     list_of_nodes[2] = [int(element) for element in list_of_nodes[2]]
     return list_of_nodes[2]
 
@@ -30,37 +36,39 @@ def n_node(list_of_nodes, length):
 #taking the input for number of nodes in the system
 n = int(input('Enter the number of nodes in a system'))
 
-#Obtaining combinations of 0 and 1 for counts
-combinations = product('01', repeat=n)
-# Convert the tuples to strings
-valid_combinations = [','.join(comb) for comb in combinations]
-
 #Obtaining combinations of 0 and 1 in integer format
-int_combinations = list(product([0, 1], repeat=n))
+int_combinations_tuple = list(product([0, 1], repeat=n))
+# Convert the tuples to lists
+int_combinations = [list(combination) for combination in int_combinations_tuple]
+str_combinations = [','.join(map(str, combination)) for combination in int_combinations_tuple]
+
 
 #Creating a datframe for counts
-states = pd.DataFrame(index=valid_combinations)
-
+states = pd.DataFrame(index=str_combinations)
 for k in range(len(int_combinations)):
     current_state = int_combinations[k]
-    # Setting counts 0 for new iteration
-    states[valid_combinations[k]] = 0 
-    state_path = []
+    states[str_combinations[k]] = 0 
     current_state_copy = current_state
-
-    # f = random.choice(func)  #check
-
     for i in range(100):
-        state_path.append(list(current_state))
+        state_path = []
         result = ','.join(map(str, current_state))
-        states.loc[result, valid_combinations[k]] += 1
+      #   states.loc[result, str_combinations[k]] += 1
         current_state = current_state_copy
         for j in range(99): 
             next_state = run(current_state) 
             result = ','.join(map(str, next_state))
-            states.loc[result, valid_combinations[k]] += 1
+            # states.loc[result, str_combinations[k]] += 1
             current_state = list(next_state)
             state_path.append(list(current_state))
+            
+            if are_last_10_elements_same(state_path):
+                states.loc[result, str_combinations[k]] += 1
+                break
+            else:
+                continue
+            
+
+
 
     # print(current_state_copy)
 print(states)
