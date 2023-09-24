eff034d8f24523387301e8e93eb440988d566b4b
TanishkPatodi <135834227+TanishkPatodi@users.noreply.github.com>
2023-09-24 18:05:03 +0530

    Updated Updated

--- Ising_Formalism_N-Node.py
+++ Ising_Formalism_N-Node.py
@@ -5,8 +5,23 @@ import pandas as pd
 import random
 # %%
 # Defining functions
-def next_state_function(current_state_matrix, edge_matrix):
-       global current_state
+random.seed(42)
+def next_state_function_Async(current_state_matrix, edge_matrix):
+    temp = list(current_state_matrix)
+    current_state_matrix_new = np.dot(edge_matrix, current_state_matrix)
+    current_state_matrix_new_2 = [
+            [-1 if element[0] < -1 else 
+            1 if element[0] > 1 else 
+            current_state_matrix[i][0] if element[0] == 0 
+            else element[0]] 
+            for i, element in enumerate(current_state_matrix_new)
+            ]
+    node = random.randint(0, len(current_state_matrix)-1)
+    temp[node] = current_state_matrix_new_2[node] 
+      
+    return temp 
+
+def next_state_function_sync(current_state_matrix, edge_matrix):
        current_state_matrix_new = np.dot(edge_matrix, current_state_matrix)
        current_state_matrix_new_2 = [
               [-1 if element[0] < -1 else 
@@ -15,11 +30,7 @@ def next_state_function(current_state_matrix, edge_matrix):
               else element[0]] 
               for i, element in enumerate(current_state_matrix_new)
               ]
-       node = random.randint(0, len(current_state)-1) #for async update
-       current_state_matrix[node] = current_state_matrix_new_2[node] # for async 
-       current_state = list(chain.from_iterable(current_state_matrix)) # for async 
-       return current_state_matrix #For async update
-      #  return current_state_matrix_new_2 #For sync update
+       return current_state_matrix_new_2 #For sync update
 
 def are_last_10_elements_same(lst):
     if len(lst) < 10:
@@ -52,18 +63,23 @@ for k in range(len(int_combinations)):
        current_state_matrix.append([element])
     # Creating new column for new iteration/state
     states[str_combinations[k]] = 0 
+    current_state_matrix_copy = list(current_state_matrix)
     current_state_copy = current_state
     for i in range(100):
         state_path = []
         result = ','.join(map(str, current_state))
+        state_path.append(list(current_state))
       #   states.loc[result, str_combinations[k]] += 1
+        current_state_matrix = current_state_matrix_copy
         current_state = current_state_copy
         for j in range(99): 
-            next_state = next_state_function(current_state_matrix, edge_matrix) 
+            next_state = next_state_function_sync(current_state_matrix, edge_matrix) 
+            current_state = list(chain.from_iterable(next_state))
             result = ','.join(str(element[0]) for element in next_state)
             # states.loc[result, str_combinations[k]] += 1
             current_state = [element[0] for element in next_state]
             state_path.append(list(current_state))
+            # print(state_path, "\n\n\n\n")
             current_state_matrix = next_state
             if are_last_10_elements_same(state_path):
                 states.loc[result, str_combinations[k]] += 1
--- N-node-toggle-SA_1D.py
+++ N-node-toggle-SA_1D.py
@@ -5,33 +5,62 @@ import random
 import pandas as pd
 from itertools import product
 #%%
+#Defining all functions
 def are_last_10_elements_same(lst):
     if len(lst) < 10:
         return False
     
     last_10_elements = lst[-10:]
     return all(element == last_10_elements[0] for element in last_10_elements)
+
+def n_node_sync(list_of_nodes):
+    # Synchronous update
+    for node in range(len(list_of_nodes[0])):
+        list_of_nodes[1] = [not element if i != node
+                                else element 
+                                for i, element in enumerate(list_of_nodes[0])]
+        # Apply AND operation between each element and the rest of the elements
+        # Use one of the two statements at a time
+        # Change any to all()/any() for async AND/OR
+
+        # list_of_nodes[2][node] = all(list_of_nodes[1][i] for i in range(len(list_of_nodes[1])) if i != node) # Without SA
+        list_of_nodes[2][node] = any(list_of_nodes[1]) #With SA
+        # Converting boolean to int
+        list_of_nodes[2] = [int(element) for element in list_of_nodes[2]]
+    return list_of_nodes[2]
+
+
+def n_node_async(list_of_nodes):
+    # Asynchronous Update
+    node = random.randint(0, len(list_of_nodes[0])-1)
+    # Apply NOT operation to every element except the one at except_index
+    list_of_nodes[1] = [not element if i != node 
+                        else element 
+                        for i, element in enumerate(list_of_nodes[0])]
+    
+    # Apply AND operation between each element and the rest of the elements
+    # Use one of the two statements at a time
+    # Change any to all()/any() for async AND/OR
+
+    # list_of_nodes[2][node] = any(list_of_nodes[1][i] for i in range(len(list_of_nodes[1])) if i != node) # Without SA
+    list_of_nodes[2][node] = all(list_of_nodes[1]) # With SA
+
+    # Converting boolean to int
+    list_of_nodes[2] = [int(element) for element in list_of_nodes[2]]
+    return list_of_nodes[2]
+
+
 #Defining function to obtain a new state
 def run(x):
     x_all = []
     np.array(x_all.append(list(x)))
     x_all.append(list(x))
     x_all.append(list(x))
-    to_return = n_node(x_all, len(x_all))
+    to_return = n_node_sync(x_all) # change for synchronous or async update
     return to_return
-#%%
-def n_node(list_of_nodes, length):
-    node = random.randint(0, len(list_of_nodes[0])-1)
-    # Apply NOT operation to every element except the one at except_index
-    list_of_nodes[1] = [not element if i != node 
-                        else element 
-                        for i, element in enumerate(list_of_nodes[0])]
-    list_of_nodes[1] = list_of_nodes[1] # For removing self activation
-    # Apply AND operation between each element and the rest of the elements
-    list_of_nodes[2][node] = all(list_of_nodes[1]) #Change any to all()/any() for async AND/OR
-    list_of_nodes[2] = [int(element) for element in list_of_nodes[2]]
-    return list_of_nodes[2]
 
+
+#%%
 #Running simulations 
 #taking the input for number of nodes in the system
 n = int(input('Enter the number of nodes in a system'))
@@ -51,6 +80,7 @@ for k in range(len(int_combinations)):
     current_state_copy = current_state
     for i in range(100):
         state_path = []
+        state_path.append(list(current_state))
         result = ','.join(map(str, current_state))
       #   states.loc[result, str_combinations[k]] += 1
         current_state = current_state_copy
@@ -59,6 +89,7 @@ for k in range(len(int_combinations)):
             result = ','.join(map(str, next_state))
             # states.loc[result, str_combinations[k]] += 1
             current_state = list(next_state)
+            # print(state_path, "\n\n\n\n")
             state_path.append(list(current_state))
             
             if are_last_10_elements_same(state_path):
